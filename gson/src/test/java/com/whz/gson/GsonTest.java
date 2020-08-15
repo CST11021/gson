aefc0fd09bba688b752beaca6c84aa2d64342a1e
wanghz <wanghz@tuya.com>
2020-08-15 17:18:03 +0800

    格式化代码

---
+++ gson/src/test/java/com/whz/gson/GsonTest.java
@@ -0,0 +1,22 @@
+package com.whz.gson;
+
+import com.google.gson.*;
+import junit.framework.TestCase;
+
+import java.lang.reflect.Type;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * @Author: wanghz
+ * @Date: 2020/8/15 5:04 PM
+ */
+public class GsonTest extends TestCase {
+
+    public void test() {
+        Gson gson = new Gson();
+
+    }
+
+}
